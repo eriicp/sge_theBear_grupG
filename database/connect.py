82a51505c6f25624776b049fa8a4e1c9640ed168
eriicp <eric.parada.r@gmail.com>
2025-02-25 08:39:16 +0100

    connect.py acabat

---
+++ database/connect.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+def connection_db():
+    conn = psycopg2.connect(
+        database="all_penjat",
+        user="user",
+        password="pass",
+        host="localhost",
+        port="5432"
+    )
+    return conn
+print (connection_db)
