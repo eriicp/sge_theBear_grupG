e6be1d164ff34257d3c9f25c23794a3c65fb0b87
eriicp <eric.parada.r@gmail.com>
2025-02-25 08:53:35 +0100

    read acabat

---
+++ services/read.py
@@ -0,0 +1,29 @@
+from schema import read_sch
+
+def registre():
+    users = {
+        "user1":{
+            "id":1,
+            "name":"Eric",
+            "surname":"Parada",
+            "age":18
+        },
+        "user2": {
+            "id": 2,
+            "name": "Hugo",
+            "surname": "Murillo",
+            "age": 18
+        },
+        "user3": {
+            "id": 3,
+            "name": "Oriol",
+            "surname": "Martinez",
+            "age": 19
+        },
+        "user4": {
+            "id": 4,
+            "name": "Itzan",
+            "surname": "Jimenez",
+            "age": 18
+        }
+    }
