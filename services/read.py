b78140956971f8fab7fd7b240e493df1b07c2839
hugomurilloo <hugomurillocaparros@gmail.com>
2025-02-25 08:50:31 +0100

    read acabat

---
+++ services/read.py
@@ -0,0 +1,30 @@
+from schema import read_sch
+
+def registrate():
+    users = {
+        "user1":{
+            "id":1,
+            "name":"Hugo",
+            "surname":"Murillo",
+            "age":"18"
+        },
+        "user2": {
+            "id": 2,
+            "name": "Eric",
+            "surname": "Parada",
+            "age": "19"
+        },
+        "user3": {
+            "id": 3,
+            "name": "Oriol",
+            "surname": "Martínez",
+            "age": "20"
+        },
+        "user4": {
+            "id": 4,
+            "name": "Mesi",
+            "surname": "Jimenez",
+            "age": "68"
+        }
+    }
+    return read_sch.schemas(users)
