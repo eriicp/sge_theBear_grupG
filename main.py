5093d93ffaabe7dfe3b6bb6579b70290cae47b51
OriolME <oriolmartinezexposito@gmail.com>
2025-02-25 09:13:14 +0100

    main.py acabat

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from typing import List
 from fastapi import FastAPI
 from services import read
 
-app = FastAP1()
+app = FastAPI()
 
-@app.get("/root", respnse_model=list[dict])
+@app.get("/root", response_model=list[dict])
 async def read_root():
     result = read.registre()
     return result
